aa33fcd6fe28c970bef9d5615fbf5d025adfec58
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-08-04 01:15:24 +0900

    初期設定完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # CSRF(クロスサイトリクエストフォージェリ) 対策
+  protect_from_forgery
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
